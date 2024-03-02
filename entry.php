d507de39745f8cd470a61a11af118727002c62fe
Vadym Buzeichuk <vv5buz@gmail.com>
2024-03-02 14:38:09 -0700

    Single page design changes
    Single page of article:
    - Position of article title has changed
    - Article date of post has changed color and margins
    - Added article source to the top of article

--- entry.php
+++ entry.php
@@ -1,15 +1,22 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php the_post_thumbnail(); ?>
-    <div class="entry-meta">
-        <time class="entry-date" datetime="<?php echo esc_attr(get_the_date('c')); ?>" title="<?php echo esc_attr(get_the_date()); ?>" <?php if (is_single()) {
-                                                                                                                                            echo 'itemprop="datePublished" pubdate';
-                                                                                                                                        } ?>><?php the_time(get_option('date_format')); ?></time>
-        <?php if (is_single()) {
-            echo '<meta itemprop="dateModified" content="' . esc_attr(get_the_modified_date()) . '" />';
-        } ?>
-    </div>
     <h1 class="entry-title" itemprop="headline">
         <?php the_title(); ?>
     </h1>
+    <div class="entry-meta">
+        <!-- The source link -->
+        <span style="color: rgb(76,76,76);">Джерело: </span>
+        <a href="<?php echo get_post_custom()['Source'][0]; ?>">CBC News</a>
+        <span> • </span>
+
+        <!-- Date of post -->
+        <time
+          class="entry-date" 
+          datetime="<?php echo esc_attr(get_the_date('c')); ?>"
+          title="<?php echo esc_attr(get_the_date()); ?>"
+          <?php if (is_single()) { echo 'itemprop="datePublished" pubdate';} ?>>
+            <?php the_time(get_option('date_format')); ?>
+        </time>
+    </div>
+    <?php the_post_thumbnail(); ?>
     <?php the_content(); ?>
 </article>
