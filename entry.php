5dc23b5653cb214fa0476552687d34448fa2db8e
Vadym Buzeichuk <vv5buz@gmail.com>
2024-07-31 16:21:08 -0600

    style: Updated style of main page and single page of the article

--- entry.php
+++ entry.php
@@ -1,37 +1,42 @@
-<a class="back-link" href="<?php echo wp_get_referer(); ?>">
-    <div>
+<?php get_header(); ?>
+<a class="row back-link" href="<?php echo home_url(); ?>">
+    <div class="offset-sm-1">
         <span class="material-symbols-outlined">arrow_back</span>
         <span class="back-link__text">Повернутись назад</span>
     </div>
 </a>
-<div class="row">
-    <div class="col-12 col-sm-9">
+<div class="row article-preview">
+    <div class="col-12 col-sm-8 offset-sm-1">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h1 class="entry-title" itemprop="headline">
                 <?php the_title(); ?>
             </h1>
             <div class="entry-meta">
                 <!-- The source link -->
-                <span style="color: rgb(76,76,76);">Джерело: </span>
-                <a href="<?php echo get_post_custom()['Source'][0]; ?>">CBC News</a>
-                <span> • </span>
-        
+                <?php if (isset(get_post_custom()['Source'][0])) { ?>
+                    <span style="color: rgb(76,76,76);">Джерело: </span>
+                    <a href="<?php echo get_post_custom()['Source'][0]; ?>">CBC News</a>
+                    <span> • </span>
+                <?php } ?>
+
                 <!-- Date of post -->
                 <!-- <time
                   class="entry-date" 
                   datetime="<?php echo esc_attr(get_the_date('c')); ?>"
                   title="<?php echo esc_attr(get_the_date()); ?>"
-                  <?php if (is_single()) { echo 'itemprop="datePublished" pubdate';} ?>>
+                  <?php if (is_single()) {
+                        echo 'itemprop="datePublished" pubdate';
+                    } ?>>
                     <?php the_time(get_option('date_format')); ?>
                 </time> -->
                 <?php
-                    $original_date = get_the_date('j F Y');
-                    $ukrainian_date = translate_month_to_ukrainian($original_date);
-                    if (is_single()) {
-                        echo '<time class="entry-date" datetime="' . esc_attr(get_the_date('c')) . '" title="' . esc_attr(get_the_date()) . '" itemprop="datePublished" pubdate>' . $ukrainian_date . '</time>';
-                    } else {
-                        echo '<time class="entry-date" datetime="' . esc_attr(get_the_date('c')) . '" title="' . esc_attr(get_the_date()) . '">' . $ukrainian_date . '</time>';
-                    }
+                $original_date = get_the_date('j F Y');
+                $ukrainian_date = translate_month_to_ukrainian($original_date);
+                if (is_single()) {
+                    echo '<time class="entry-date" datetime="' . esc_attr(get_the_date('c')) . '" title="' . esc_attr(get_the_date()) . '" itemprop="datePublished" pubdate>' . $ukrainian_date . '</time>';
+                } else {
+                    echo '<time class="entry-date" datetime="' . esc_attr(get_the_date('c')) . '" title="' . esc_attr(get_the_date()) . '">' . $ukrainian_date . '</time>';
+                }
                 ?>
             </div>
             <?php the_post_thumbnail(); ?>
@@ -44,7 +49,7 @@
             <p>Читайте новини Калгарі українською у короткому форматі телеграмм каналу</p>
             <a href="https://t.me/+iA5u8nQs-9hlN2Yy">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
                 </svg>
                 <span>@calgariivka</span>
             </a>
--- index.php
+++ index.php
@@ -1,39 +1,89 @@
 <?php get_header(); ?>
+
+<!-- BIG POST -->
 <div>
-    <?php
-    $query_guides = new WP_Query(array('category_name' => 'guides', "post_count" => 1));
-    if ($query_guides->have_posts()) {
-        echo '<h1>Посібники</h1>';
-    } ?>
+    <h1>Останні новини</h1>
     <div class="row article-preview">
         <?php
-        if ($query_guides->have_posts()) : while ($query_guides->have_posts()) : $query_guides->the_post(); ?>
-                <?php get_template_part('preview'); ?>
-        <?php
-            endwhile;
-        endif;
-        ?>
-    </div>
-</div>
-<div>
-    <h1>Новини</h1>
-    <div class="row article-preview">
-        <?php
-        $query_news = new WP_Query(array('category_name' => 'news'));
-        $i = 0;
-        if ($query_news->have_posts()) : while ($query_news->have_posts()) : $query_news->the_post();
-            $class = ($i == 0) ? 'col-lg-12 col-12 article-preview__element-1' : 'col-lg-4 col-12 article-preview__element';
-            ?>
-                <div class="<?= $class ?>">
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 1,
+            'tax_query' => array(
+                array(
+                    'category_name' => 'news',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                ?>
+                <div class="col-lg-12 col-12 article-preview__element-1">
                     <?php get_template_part('preview'); ?>
                 </div>
-            <?php
-            $i++;
-        endwhile;
-        endif;
+            <?php }
+        }
+        wp_reset_postdata();
         ?>
     </div>
 </div>
 
+<!-- SMALL POSTS -->
+<div>
+    <div class="row article-preview" style="border-bottom: 1px solid rgba(0, 0, 0, 0.1); margin-bottom: 16px;">
+        <?php
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 4,
+            'offset' => 1,
+            'tax_query' => array(
+                array(
+                    'category_name' => 'news',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                ?>
+                <div class="col-lg-3 col-12 article-preview__element-min">
+                    <?php get_template_part('preview'); ?>
+                </div>
+            <?php }
+        }
+        wp_reset_postdata();
+        ?>
+    </div>
+</div>
+
+<!-- OTHER POSTS -->
+<div>
+    <div class="row article-preview">
+        <?php
+        $args_other = array(
+            'post_type' => 'post',
+            'posts_per_page' => 20,
+            'offset' => 5,
+            'tax_query' => array(
+                array(
+                    'category_name' => 'news',
+                ),
+            ),
+        );
+        $query_other = new WP_Query($args_other);
+        if ($query_other->have_posts()) {
+            while ($query_other->have_posts()) {
+                $query_other->the_post();
+                ?>
+                <div class="col-lg-4 col-12 article-preview__element">
+                    <?php get_template_part('preview'); ?>
+                </div>
+            <?php }
+        }
+        wp_reset_postdata();
+        ?>
+</div>
 
 <?php get_footer();
