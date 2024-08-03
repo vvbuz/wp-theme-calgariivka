7937ee248c6005d641e807d032741897813d8a31
Vadym Buzeichuk <vv5buz@gmail.com>
2024-08-03 15:43:57 -0600

    style: Updated 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,20 @@
-<!-- TODO: Start from the scratch -->
-
 <?php get_header(); ?>
-    <h1 style="text-align: center; font-size: 5em !important;">404</h1>
-    <h2>Сторінка не існує – може поки що, а може вже.</h2>
-    <p style="text-align: center;"><a href="/">F*ck, go back</a></p>
+
+<style>
+    @media screen and (max-width: 768px) {
+        .four-o-four {
+            height: 50vh !important;
+        }
+        h1 {
+            padding-top: 13vh !important;
+        }
+    }
+</style>
+
+<div class="four-o-four" style="height: 60vh;">
+    <h1 style="text-align: center; font-size: 5em !important; padding-top: 20vh;">404</h1>
+    <h2 style="text-align: center;">Сторінка не існує – може поки що, а може вже.</h2>
+    <p style="text-align: center;"><a href="/">Дідько, повернутись на головну</a></p>
+</div>
+
 <?php get_footer(); ?>
